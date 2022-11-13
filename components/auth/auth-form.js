3affe31e6bfabf677941328a012cd3e185eb4205
rawwr <akshayace0302@gmail.com>
2022-11-14 01:18:37 +0530

    unique email address

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,25 +1,81 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import classes from "./auto-form.module.css";
 
-function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
-  console.log(isLogin);
+const createUser = async (email, password) => {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    //Converts a JavaScript value to (JSON) string.
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-  function switchAuthModeHandler() {
-    setIsLogin((prevState) => !prevState);
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong!");
   }
+  return data;
+};
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  //   console.log(isLogin);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  //since createUser is async, thus we have to handle the promise
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // add validation
+
+    if (isLogin) {
+      // ...
+      //in case of not login i.e signup
+    } else {
+      try {
+        const result = await createUser(enteredEmail, enteredPassword);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required autoComplete="on" />
+          <input
+            type="email"
+            id="email"
+            required
+            autoComplete="on"
+            ref={emailInputRef}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required autoComplete="on" />
+          <input
+            type="password"
+            id="password"
+            required
+            autoComplete="on"
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? "Login" : "Create Account"}</button>
@@ -34,6 +90,6 @@ function AuthForm() {
       </form>
     </section>
   );
-}
+};
 
 export default AuthForm;
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,26 +1,42 @@
 import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
-const Handler = async (req, res) => {
-  const { email, password } = req.body;
 
-  if (
-    !email ||
-    !email.includes("@") ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res.status(422).json({ message: "Invalid input - password too short!" });
+const Handler = async (req, res) => {
+  if (req.method !== "POST") {
     return;
   }
-  const client = await connectToDatabase();
-  const db = client.db();
 
-  const hashedPassword = hashPassword(password);
-  const result = await db
-    .collection("users")
-    .insertOne({ email: email, password: hashedPassword });
-  console.log(result);
-  res.status(201).json({ message: "Created user!" });
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({ message: "Invalid input - password too short!" });
+      return;
+    }
+    const client = await connectToDatabase();
+    const db = client.db();
+
+    const existingUser = await db.collection("users").findOne({ email: email });
+
+    if (existingUser) {
+      res.status(422).json({ message: "User exists already!" });
+      client.close();
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+    const result = await db
+      .collection("users")
+      .insertOne({ email: email, password: hashedPassword });
+    console.log(result);
+
+    res.status(201).json({ message: "Created user!" });
+    client.close();
+  }
 };
 
 export default Handler;
